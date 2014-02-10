9e9b7a8a8a8a2cd3b3b3f87e6d31b5fb7f9f2150
James Holmes <32bitkid@gmail.com>
2014-02-10 12:42:34 -0500

    Adding more tasks for compiling and minifiying the source, as well as watches.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,35 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
   grunt.initConfig({
+    watch: {
+      scripts: {
+        files: ['V1.Backbone.coffee', 'tests/**/*.coffee'],
+        tasks: ["mochaTest", "coffee:compile", 'uglify'],
+        options: { spawn: true }
+      }
+    },
+
+    coffee: {
+      compile: {
+        files: {
+          'V1.Backbone.js': 'V1.Backbone.coffee'
+        }
+      }
+    },
+
+    uglify: {
+      scripts: {
+        files: {
+          'V1.Backbone.min.js': ['V1.Backbone.js']
+        }
+      }
+    },
+
     mochaTest: {
       test: {
         options: {
